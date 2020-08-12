ceec900bcf7a269c8e7a6783d31b5c83a129a663
Valeria Ramey <valeriaramey@gmail.com>
2020-08-12 19:56:04 -0400

    index cards created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating flats...'
+Flat.destroy_all
+5.times do
+Flat.create(name: Faker::Coffee.blend_name, address: Faker::Address.street_address, price_per_night: rand(25..100), number_of_guests: rand(1..10),
+            description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true))
+end
